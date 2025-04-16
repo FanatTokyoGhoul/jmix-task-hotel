a2b87ef62e436bb39d61cf464c88a46794f18274
Maxim <nikitin@redcollar.ru>
2025-04-16 07:13:37 +0300

    improve load client with email

--- src/main/java/com/company/homeworkhotel/view/roomreservation/ReservedRoomsList.java
+++ src/main/java/com/company/homeworkhotel/view/roomreservation/ReservedRoomsList.java
@@ -5,12 +5,18 @@ import com.company.homeworkhotel.entity.RoomReservation;
 import com.company.homeworkhotel.view.main.MainView;
 import com.vaadin.flow.router.Route;
 import io.jmix.core.DataManager;
+import io.jmix.core.FetchPlan;
+import io.jmix.core.FetchPlans;
 import io.jmix.core.Id;
+import io.jmix.data.PersistenceHints;
 import io.jmix.flowui.Dialogs;
 import io.jmix.flowui.component.grid.DataGrid;
 import io.jmix.flowui.kit.action.ActionPerformedEvent;
 import io.jmix.flowui.view.*;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Route(value = "reserved-rooms", layout = MainView.class)
 @ViewController("ReservedRooms.list")
@@ -24,6 +30,8 @@ public class ReservedRoomsList extends StandardListView<RoomReservation> {
     private Dialogs dialogs;
     @Autowired
     private DataManager dataManager;
+    @Autowired
+    private FetchPlans fetchPlans;
 
     @Subscribe("roomReservationsDataGrid.viewClientEmail")
     public void onRoomReservationsDataGridViewClientEmail(final ActionPerformedEvent event) {
@@ -33,8 +41,14 @@ public class ReservedRoomsList extends StandardListView<RoomReservation> {
         }
         Client client = reservation.getBooking().getClient();
 
+        FetchPlan fetchPlan = fetchPlans.builder(Client.class)
+                .add("email")
+                .build();
+
         String email = dataManager.load(Id.of(client.getId(), Client.class))
-                .one().getEmail();
+                .fetchPlan(fetchPlan)
+                .one()
+                .getEmail();
 
         dialogs.createMessageDialog()
                 .withHeader("Client email")
